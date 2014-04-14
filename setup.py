de07a55f73bc5cbe246413f8b1c2805e8bf2b3a8
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-04-14 18:57:24 -0300

    Fiz package name

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
-    name='dbaas-cloudstack',
+    name='dbaas_cloudstack',
     version=version,
     description="""A cloudstack adapter to DBaaS""",
     long_description=readme + '\n\n' + history,
