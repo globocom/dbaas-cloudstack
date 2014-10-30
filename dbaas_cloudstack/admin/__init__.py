5b8c9cd6b4193aa85c3c4d1e8d42a2cdc0751919
Felippe da Motta Raposo <raposo.felippe@gmail.com>
2014-10-30 12:12:45 -0200

    Add region admin

--- dbaas_cloudstack/admin/__init__.py
+++ dbaas_cloudstack/admin/__init__.py
@@ -2,7 +2,9 @@
 from django.contrib import admin
 from .cloudstack_serviceoffering import CloudStackServiceofferingAdmin
 from .cloudstack_bundle import CloudStackBundleAdmin
+from .cloudstack_region import CloudStackRegionAdmin
 from .. import models
 
 admin.site.register(models.CloudStackOffering, CloudStackServiceofferingAdmin)
 admin.site.register(models.CloudStackBundle, CloudStackBundleAdmin)
+admin.site.register(models.CloudStackRegion, CloudStackRegionAdmin)
