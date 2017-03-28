40973299b5d4adfec44f5ecc2c319e6559bb3110
Ricardo Dias <ricardo@ricardodias.org>
2017-03-28 17:57:25 -0300

    new function get_cs_credential

--- dbaas_cloudstack/util/__init__.py
+++ dbaas_cloudstack/util/__init__.py
@@ -1,4 +1,15 @@
 from django.contrib.auth.models import User
 
+
 def make_db_random_password():
     return User.objects.make_random_password()
+
+
+def get_cs_credential(environment):
+    from dbaas_credentials.models import Credential
+    from dbaas_credentials.models import CredentialType
+
+    return Credential.objects.filter(
+        integration_type__type=CredentialType.CLOUDSTACK,
+        environments=environment)[0]
+    return
