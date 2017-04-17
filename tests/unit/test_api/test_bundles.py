6a66e345bbaaee45cd80c0f4163d2a3530925728
Elesbom <rafael.goncalves@corp.globo.com>
2017-04-17 16:59:31 -0300

    Finish tests of bundles api

--- tests/unit/test_api/test_bundles.py
+++ tests/unit/test_api/test_bundles.py
@@ -5,13 +5,13 @@ from unittest import TestCase
 from django.test import Client
 
 
+@patch('dbaas_cloudstack.api.views.CloudStackBundle.objects.filter')
 class GetTestCase(TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.client = Client()
 
-    @patch('dbaas_cloudstack.api.views.CloudStackBundle.objects.filter')
     def test_pass_id_from_get(self, filter_mock):
         resp = self.client.get('/api/bundles/999/')
 
@@ -19,10 +19,9 @@ class GetTestCase(TestCase):
         self.assertTrue(filter_mock.called)
         self.assertEqual(filter_mock.call_args[1]['engine_id'], '999')
 
-    @patch('dbaas_cloudstack.api.views.CloudStackBundle.objects.filter.values')
-    def test_return_status_200_when_no_bundles_found(self, values_mock):
+    def test_return_status_200_when_no_bundles_found(self, filter_mock):
 
-        values_mock.return_value = []
+        filter_mock.return_value = type('nada', (), {'values': lambda *args, **kw: []})()
         resp = self.client.get('/api/bundles/33/')
 
         self.assertEqual(resp.status_code, 200)
