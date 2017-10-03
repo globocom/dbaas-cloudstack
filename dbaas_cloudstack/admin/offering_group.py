59cede51342a988b957c8ddf28c1dc8b5e770066
anita.vincent <anita.vincent@mac186080.local>
2017-10-03 14:23:41 -0300

    Refactor offeringgroup admin class

--- dbaas_cloudstack/admin/offering_group.py
+++ dbaas_cloudstack/admin/offering_group.py
@@ -1,60 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 from dbaas_cloudstack.models import CloudStackRegion
 from dbaas_cloudstack.models import CloudStackOffering
+from dbaas_cloudstack.util.admin_filters import BaseGroupFilter
 
-class RegionListFilter(admin.SimpleListFilter):
-    # Human-readable title which will be displayed in the
-    # right admin sidebar just above the filter options.
-    title = _('region')
+class RegionListFilter(BaseGroupFilter):
+    title = 'region'
 
-    # Parameter for the filter that will be used in the URL query.
     parameter_name = 'region'
 
-    def lookups(self, request, model_admin):
+    filter_option_queryset = CloudStackRegion.objects.values_list("name", "name")
 
-        tuple_list = CloudStackRegion.objects.values_list("name", "name")
+    def get_acceptable_ids(self, value):
 
-        return (
-            list(tuple_list)
-        )
-
-    def queryset(self, request, queryset):
-
-        if not self.value():
-            return
-
-        offerings_list = CloudStackOffering.objects.filter(region__name=self.value())
+        offerings_list = CloudStackOffering.objects.filter(region__name=value)
         acceptable_ids = []
 
         for offering in offerings_list:
             acceptable_ids.extend(offering.offeringgroup_set.values_list("id", flat=True))
 
-        return queryset.filter(id__in=set(acceptable_ids))
+        return acceptable_ids
 
 
-class EnvironmentListFilter(admin.SimpleListFilter):
-    # Human-readable title which will be displayed in the
-    # right admin sidebar just above the filter options.
-    title = _('environment')
+class EnvironmentListFilter(BaseGroupFilter):
+    title = 'environment'
 
-    # Parameter for the filter that will be used in the URL query.
     parameter_name = 'environment'
 
-    def lookups(self, request, model_admin):
+    filter_option_queryset = CloudStackRegion.objects.values_list("environment__name", "environment__name")
 
-        tuple_list = CloudStackRegion.objects.values_list("environment__name", "environment__name")
-
-        return (
-            list(tuple_list)
-        )
-
-    def queryset(self, request, queryset):
-
-        if not self.value():
-            return
+    def get_acceptable_ids(self, value):
 
         offerings_list = CloudStackOffering.objects.filter(region__environment__name=self.value())
         acceptable_ids = []
@@ -62,10 +38,9 @@ class EnvironmentListFilter(admin.SimpleListFilter):
         for offering in offerings_list:
             acceptable_ids.extend(offering.offeringgroup_set.values_list("id", flat=True))
 
-        return queryset.filter(id__in=set(acceptable_ids))
+        return acceptable_ids
         
 
 
 class OfferingGroupAdmin(admin.ModelAdmin):
-    #list_display = ("name",)
     list_filter = (RegionListFilter, EnvironmentListFilter)
